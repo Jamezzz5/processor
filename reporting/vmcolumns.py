a057fb018f6c8029e853b70eadb267dfdae5c951
Jamezzz5 <imirzian@umich.edu>
2017-11-28 13:30:45 -0500

    added retention metrics

--- reporting/vmcolumns.py
+++ reporting/vmcolumns.py
@@ -34,6 +34,11 @@ frequency = 'Frequency'
 engagements = 'Engagements'
 likes = 'Likes'
 revenue = 'Revenue'
+ret_day1 = 'Retention - Day 1'
+ret_day3 = 'Retention - Day 3'
+ret_day7 = 'Retention - Day 7'
+ret_day14 = 'Retention - Day 14'
+ret_day30 = 'Retention - Day 30'
 landingpage = 'Landing Page'
 homepage = 'Homepage'
 btnclick = 'Button Click'
@@ -87,15 +92,15 @@ vmkeys = [filename, firstrow, lastrow, fullplacename, placement, filenamedict,
           apifile, apifields, apimerge, transform, header, omit_plan]
 
 datacol = [date, impressions, clicks, cost, views, views25, views50, views75,
-           views100, reach, frequency, engagements, likes, revenue,
-           landingpage, homepage, btnclick, purchase, signup, newuser,
-           activeuser, download, login, gplay, gplay3, gplay6, landingpagepi,
-           homepagepi, btnclickpi, purchasepi, signuppi, newuserpi,
-           activeuserpi, downloadpi, loginpi, gplaypi, gplay3pi, gplay6pi,
-           landingpagepc, homepagepc, btnclickpc, purchasepc, signuppc,
-           newuserpc, activeuserpc, downloadpc, loginpc, gplaypc, gplay3pc,
-           gplay6pc, conv1, conv2, conv3, conv4, conv5, conv6, conv7, conv8,
-           conv9, conv10]
+           views100, reach, frequency, engagements, likes, revenue, ret_day1,
+           ret_day3, ret_day7, ret_day14, ret_day30, landingpage, homepage,
+           btnclick, purchase, signup, newuser, activeuser, download, login,
+           gplay, gplay3, gplay6, landingpagepi, homepagepi, btnclickpi,
+           purchasepi, signuppi, newuserpi, activeuserpi, downloadpi, loginpi,
+           gplaypi, gplay3pi, gplay6pi, landingpagepc, homepagepc, btnclickpc,
+           purchasepc, signuppc, newuserpc, activeuserpc, downloadpc, loginpc,
+           gplaypc, gplay3pc, gplay6pc, conv1, conv2, conv3, conv4, conv5,
+           conv6, conv7, conv8, conv9, conv10]
 
 vmkeys += datacol
 barsplitcol = ([fullplacename, dropcol, autodicord, apifields] + datacol)
