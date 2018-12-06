2565009dc81c4126cb5bc61046832d6dc9dfe57a
Jamezzz5 <imirzian@umich.edu>
2018-12-06 17:32:54 -0500

    added metric cap class for calc

--- reporting/calc.py
+++ reporting/calc.py
@@ -38,14 +38,14 @@ PLACE_DATE = 'Placement Date'
 DIF_PNC = 'Dif - PNC'
 DIF_NC = 'Dif - NC'
 DIF_NC_PNC = 'Dif - NC/PNC'
-DIF_COL = [dctc.PFPN, DIF_PNC, DIF_NC, DIF_NC_PNC]
+DIF_COL = [DIF_PNC, DIF_NC, DIF_NC_PNC]
 
 NC_CUM_SUM = 'Net Cost CumSum'
 NC_SUM_DATE = 'Net Cost Sum Date'
 NC_CUM_SUM_MIN_DATE = 'NC_CUMSUMMINDATE'
 
-NC_CUM_SUM_COL = [dctc.PFPN, vmc.date, NC_CUM_SUM]
-NC_SUM_DATE_COL = [dctc.PFPN, vmc.date, NC_SUM_DATE]
+NC_CUM_SUM_COL = [vmc.date, NC_CUM_SUM]
+NC_SUM_DATE_COL = [vmc.date, NC_SUM_DATE]
 
 DROP_COL = ([CLI_PD, NC_CUM_SUM, NC_SUM_DATE, PLACE_DATE,
              NC_CUM_SUM_MIN_DATE] + DIF_COL)
@@ -132,61 +132,61 @@ def net_cost_calculation(df):
     return df
 
 
-def net_plan_comp(df):
+def net_plan_comp(df, p_col=dctc.PFPN, n_cost=vmc.cost, p_cost=dctc.PNC):
     nc_pnc = df[df[dctc.UNC] != True]
-    nc_pnc = nc_pnc.groupby(dctc.PFPN)[dctc.PNC, vmc.cost].sum()
-    if dctc.PNC not in nc_pnc.columns:
-        nc_pnc[dctc.PNC] = 0
-    nc_pnc[DIF_NC_PNC] = nc_pnc[vmc.cost] - nc_pnc[dctc.PNC]
+    nc_pnc = nc_pnc.groupby(p_col)[p_cost, n_cost].sum()
+    if p_cost not in nc_pnc.columns:
+        nc_pnc[p_cost] = 0
+    nc_pnc[DIF_NC_PNC] = nc_pnc[n_cost] - nc_pnc[p_cost]
     nc_pnc = nc_pnc.reset_index()
-    nc_pnc.columns = DIF_COL
-    df = df.merge(nc_pnc, on=dctc.PFPN, how='left')
+    nc_pnc.columns = [p_col] + DIF_COL
+    df = df.merge(nc_pnc, on=p_col, how='left')
     return df
 
 
-def net_cum_sum(df):
-    nc_cum_sum = (df.groupby([dctc.PFPN, vmc.date])[vmc.cost].sum()
+def net_cum_sum(df, p_col=dctc.PFPN, n_cost=vmc.cost):
+    nc_cum_sum = (df.groupby([p_col, vmc.date])[n_cost].sum()
                   .groupby(level=[0]).cumsum()).reset_index()
-    nc_cum_sum.columns = NC_CUM_SUM_COL
-    df = df.merge(nc_cum_sum, on=[dctc.PFPN, vmc.date], how='left')
+    nc_cum_sum.columns = [p_col] + NC_CUM_SUM_COL
+    df = df.merge(nc_cum_sum, on=[p_col, vmc.date], how='left')
     return df
 
 
-def net_sum_date(df):
-    nc_sum_date = (df.groupby([dctc.PFPN, vmc.date])[vmc.cost]
-                   .sum().reset_index())
-    nc_sum_date.columns = NC_SUM_DATE_COL
-    df = df.merge(nc_sum_date, on=[dctc.PFPN, vmc.date], how='left')
+def net_sum_date(df, p_col=dctc.PFPN, n_cost=vmc.cost):
+    nc_sum_date = df.groupby([p_col, vmc.date])[n_cost].sum().reset_index()
+    nc_sum_date.columns = [p_col] + NC_SUM_DATE_COL
+    df = df.merge(nc_sum_date, on=[p_col, vmc.date], how='left')
     return df
 
 
-def net_cost_final(df):
-    nc_cum_sum_min = (df[df[NC_CUM_SUM] > df[DIF_PNC]].groupby([dctc.PFPN]).
-                      min().reset_index())
-    if not nc_cum_sum_min.empty:
-        nc_cum_sum_min = nc_cum_sum_min[[vmc.date, dctc.PFPN]]
-        nc_cum_sum_min[NC_CUM_SUM_MIN_DATE] = True
-        df = df.merge(nc_cum_sum_min, on=[dctc.PFPN, vmc.date], how='left')
+def net_cost_final(df, p_col=dctc.PFPN, n_cost=vmc.cost):
+    tdf = (df[df[NC_CUM_SUM] > df[DIF_PNC]].groupby([p_col]).
+           min().reset_index())
+    if not tdf.empty:
+        tdf = tdf[[vmc.date, p_col]]
+        tdf[NC_CUM_SUM_MIN_DATE] = True
+        df = df.merge(tdf, on=[p_col, vmc.date], how='left')
         df[NCF] = np.where(df[NC_CUM_SUM] > df[DIF_PNC],
                            (np.where(df[NC_CUM_SUM_MIN_DATE] == True,
-                                     (df[vmc.cost] -
-                                      (df[vmc.cost] / (df[NC_SUM_DATE])) *
+                                     (df[n_cost] -
+                                      (df[n_cost] / (df[NC_SUM_DATE])) *
                                       (df[NC_CUM_SUM] - df[DIF_PNC])),
                                      0)),
                            df[vmc.cost])
     else:
         df[NC_CUM_SUM_MIN_DATE] = 0
-        df[NCF] = df[vmc.cost]
+        df[NCF] = df[n_cost]
     df = utl.col_removal(df, 'Raw Data', DROP_COL)
     return df
 
 
-def net_cost_final_calculation(df):
+def net_cost_final_calculation(df, p_col=dctc.PFPN, n_cost=vmc.cost,
+                               p_cost=dctc.PNC):
     logging.info('Calculating Net Cost Final')
-    df = net_plan_comp(df)
-    df = net_cum_sum(df)
-    df = net_sum_date(df)
-    df = net_cost_final(df)
+    df = net_plan_comp(df, p_col=p_col, n_cost=n_cost, p_cost=p_cost)
+    df = net_cum_sum(df, p_col=p_col, n_cost=n_cost)
+    df = net_sum_date(df, p_col=p_col, n_cost=n_cost)
+    df = net_cost_final(df, p_col=p_col, n_cost=n_cost)
     return df
 
 
@@ -221,10 +221,54 @@ def total_cost_calculation(df):
     return df
 
 
+class MetricCap(object):
+    def __init__(self, config_file='config/cap_config.csv'):
+        self.file_name = 'file_name'
+        self.file_dim = 'file_dim'
+        self.file_metric = 'file_metric'
+        self.proc_dim = 'processor_dim'
+        self.proc_metric = 'processor_metric'
+        self.temp_metric = None
+        self.config = utl.import_read_csv(config_file)
+        self.config = self.config.to_dict(orient='index')
+
+    def get_cap_file(self, c):
+        pdf = utl.import_read_csv(c[self.file_name])
+        p_dict = self.col_dict(c)
+        pdf = pdf.rename(columns=p_dict)
+        return pdf
+
+    def col_dict(self, c):
+        self.temp_metric = c[self.proc_metric] + ' - TEMP'
+        p_dict = {c[self.file_dim]: c[self.proc_dim],
+                  c[self.file_metric]: self.temp_metric}
+        return p_dict
+
+    def apply_cap(self, df, c):
+        logging.info('Calculating metric cap from: '
+                     '{}'.format(c[self.file_name]))
+        pdf = self.get_cap_file(c)
+        df = df.append(pdf)
+        df = net_cost_final_calculation(df, p_col=c[self.proc_dim],
+                                        p_cost=self.temp_metric)
+        df = df[~df[dctc.FPN].isnull()]
+        df[vmc.cost] = df[NCF]
+        df = utl.col_removal(df, 'Raw Data', [self.temp_metric])
+        return df
+
+    def apply_all_caps(self, df):
+        if self.config:
+            for cfg in self.config:
+                c = self.config[cfg]
+                df = self.apply_cap(df, c)
+        return df
+
+
 def calculate_cost(df):
     if vmc.cost not in df.columns:
         df[vmc.cost] = 0
     df = net_cost_calculation(df)
+    df = MetricCap().apply_all_caps(df)
     df = net_cost_final_calculation(df)
     df = agency_fees_calculation(df)
     df = total_cost_calculation(df)
