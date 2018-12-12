41a0cbb36415a11697fc3c70701af378cc727305
Jamezzz5 <imirzian@umich.edu>
2018-12-11 19:14:42 -0500

    allow translations based on other col selection

--- reporting/dictionary.py
+++ reporting/dictionary.py
@@ -340,21 +340,45 @@ class DictTranslationConfig(object):
             if col not in data_dict.columns:
                 continue
             tdf = self.df[self.df[dctc.DICT_COL_NAME] == col]
-            if dctc.DICT_COL_FUNC in tdf.columns:
+            if dctc.DICT_COL_FNC in tdf.columns:
                 data_dict = self.strip_dict(tdf, col, data_dict)
-                tdf = tdf[tdf[dctc.DICT_COL_FUNC].isnull()]
-            tdf = tdf[[dctc.DICT_COL_VALUE, dctc.DICT_COL_NVALUE]]
-            replace_dict = dict(zip(tdf[dctc.DICT_COL_VALUE],
-                                    tdf[dctc.DICT_COL_NVALUE]))
-            data_dict[col] = data_dict[col].astype('U').replace(replace_dict)
+                data_dict = self.select_translation(tdf, col, data_dict)
+                tdf = tdf[tdf[dctc.DICT_COL_FNC].isnull()]
+            data_dict = self.apply_translation(tdf, col, data_dict)
+        return data_dict
+
+    @staticmethod
+    def select_translation(tdf, col, data_dict):
+        if dctc.DICT_COL_SEL not in tdf.columns:
+            return data_dict
+        tdf = tdf[tdf[dctc.DICT_COL_FNC].str.contains('Select', na=False)]
+        tdf[dctc.DICT_COL_FNC] = tdf[dctc.DICT_COL_FNC].str.split('::').str[1]
+        sel = tdf[[dctc.DICT_COL_FNC, dctc.DICT_COL_SEL, dctc.DICT_COL_VALUE,
+                   dctc.DICT_COL_NVALUE]].to_dict(orient='index')
+        for s in sel:
+            col2 = sel[s][dctc.DICT_COL_FNC]
+            col2_q = sel[s][dctc.DICT_COL_SEL]
+            val = sel[s][dctc.DICT_COL_VALUE]
+            nval = sel[s][dctc.DICT_COL_NVALUE]
+            print(s)
+            data_dict.loc[(data_dict[col2] == col2_q) &
+                          (data_dict[col] == val), col] = nval
         return data_dict
 
     @staticmethod
     def strip_dict(tdf, col, data_dict):
-        tdf = tdf[tdf[dctc.DICT_COL_FUNC] == 'Strip']
+        tdf = tdf[tdf[dctc.DICT_COL_FNC] == 'Strip']
         data_dict = utl.data_to_type(data_dict, str_col=[col])
         for val in tdf[dctc.DICT_COL_VALUE].unique():
             data_dict[col] = data_dict[col].str.strip(val)
+        return data_dict
+
+    @staticmethod
+    def apply_translation(tdf, col, data_dict):
+        tdf = tdf[[dctc.DICT_COL_VALUE, dctc.DICT_COL_NVALUE]]
+        replace_dict = dict(zip(tdf[dctc.DICT_COL_VALUE],
+                                tdf[dctc.DICT_COL_NVALUE]))
+        data_dict[col] = data_dict[col].astype('U').replace(replace_dict)
         return data_dict
 
 
