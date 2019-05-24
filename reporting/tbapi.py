b926f325465d140432ec3d59a730521be0bbdc0f
Jamezzz5 <imirzian@umich.edu>
2019-05-24 13:47:54 -0400

    catch response errors in tbapi

--- reporting/tbapi.py
+++ reporting/tbapi.py
@@ -8,10 +8,12 @@ import reporting.utils as utl
 
 config_path = utl.config_path
 
-signin_url = 'https://us-east-1.online.tableau.com/api/3.3/auth/signin'
-
 
 class TabApi(object):
+    base_url = 'https://us-east-1.online.tableau.com/api/'
+    version = '3.3'
+    base_url = '{}{}/'.format(base_url, version)
+
     def __init__(self, config_file='tabconfig.json'):
         self.config_file = config_file
         self.config = None
@@ -63,39 +65,70 @@ class TabApi(object):
                 "name": self.username,
                 "password": self.password,
                 "site": {"contentUrl": self.site}}}
-        r = requests.post(signin_url, json=data, headers=self.headers)
+        url = self.create_url('auth/signin')
+        r = self.make_request(url, 'post', resp_key='credentials',
+                              json_data=data)
         session_id = r.json()['credentials']['token']
         self.headers['X-Tableau-Auth'] = session_id
         self.site_id = r.json()['credentials']['site']['id']
 
+    def create_url(self, addition_url, site_url=False):
+        url = self.base_url
+        if site_url:
+            url = '{}sites/{}/'.format(url, self.site_id)
+        url = '{}{}'.format(url, addition_url)
+        return url
+
+    def make_request(self, url, request_type='get', resp_key=None,
+                     json_data=None, attempt=1):
+        if request_type == 'get':
+            r = requests.get(url, headers=self.headers)
+        else:
+            r = requests.post(url, json=json_data, headers=self.headers)
+        if resp_key and resp_key not in r.json():
+            logging.warning('Attempt {}, {} not in response: '
+                            '{}'.format(attempt, resp_key, r.json()))
+            time.sleep(10)
+            attempt += 1
+            if attempt < 10:
+                r = self.make_request(url, request_type, resp_key,
+                                      json_data=json_data, attempt=attempt)
+            else:
+                logging.warning('Maximum attempts exceeded - stopping.')
+                sys.exit(0)
+        return r
+
     def find_datasource(self):
-        ds_url = ('https://online.tableau.com/api/3.3/sites/'
-                  '{}/datasources?pageSize=1000'.format(self.site_id))
-        r = requests.get(ds_url, headers=self.headers)
+        logging.info('Finding datasource: {}.'.format(self.datasource))
+        ds_url = 'datasources?pageSize=1000'
+        ds_url = self.create_url(ds_url, site_url=True)
+        r = self.make_request(ds_url, resp_key='datasources')
         ds_id = [x['id'] for x in r.json()['datasources']['datasource']
                  if x['name'] == self.datasource][0]
         return ds_id
 
     def find_extract_refreshes(self, ds_id):
-        er_url = ('https://online.tableau.com/api/3.3/sites/{}/tasks/'
-                  'extractRefreshes'.format(self.site_id))
-        r = requests.get(er_url, headers=self.headers)
+        logging.info('Finding extract refresh id: {}.'.format(ds_id))
+        er_url = 'tasks/extractRefreshes'
+        er_url = self.create_url(er_url, site_url=True)
+        r = self.make_request(er_url, resp_key='tasks')
         er_id = [x['extractRefresh']['id'] for x in r.json()['tasks']['task']
                  if 'datasource' in x['extractRefresh'] and
                  x['extractRefresh']['datasource']['id'] == ds_id][0]
         return er_id
 
     def send_refresh_request(self, er_id):
-        rr_url = ('https://us-east-1.online.tableau.com/api/3.3/sites/{}/tasks/'
-                  'extractRefreshes/{}/runNow'.format(self.site_id, er_id))
-        r = requests.post(rr_url, json={}, headers=self.headers)
+        logging.info('Sending refresh request for id: {}'.format(er_id))
+        rr_url = 'tasks/extractRefreshes/{}/runNow'.format(er_id)
+        rr_url = self.create_url(rr_url, site_url=True)
+        r = self.make_request(rr_url, 'post', resp_key='job', json_data={})
         job_id = r.json()['job']['id']
         return job_id
 
     def check_job(self, job_id):
-        job_url = ('https://online.tableau.com/api/3.3/sites/'
-                   '{}/jobs/{}'.format(self.site_id, job_id))
-        r = requests.get(job_url, headers=self.headers)
+        job_url = 'jobs/{}'.format(job_id)
+        job_url = self.create_url(job_url, site_url=True)
+        r = self.make_request(job_url, resp_key='job')
         logging.info('Job request response as follows {}'.format(r.json()))
         if 'completedAt' in r.json()['job']:
             logging.info('Extract refresh completed at {}'
