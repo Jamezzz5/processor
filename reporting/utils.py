2891a366cc29e2dacfd5b7e0826c192b77c882a0
Jamezzz5 <imirzian@umich.edu>
2019-06-21 14:01:33 -0400

    allow exceptions for drop all columns

--- reporting/utils.py
+++ reporting/utils.py
@@ -153,9 +153,10 @@ def date_removal(df, date_col_name, start_date, end_date):
 
 def col_removal(df, key, removal_cols, warn=True):
     logging.debug('Dropping unnecessary columns')
-    if removal_cols == ['ALL']:
+    if 'ALL' in removal_cols:
         removal_cols = [x for x in df.columns
-                        if x not in dctc.COLS + vmc.datacol + vmc.ad_rep_cols]
+                        if x not in dctc.COLS + vmc.datacol + vmc.ad_rep_cols +
+                        removal_cols]
     for col in removal_cols:
         if col not in df:
             if col == 'nan':
