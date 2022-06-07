df24798376145a88fff2eb71190563cf25f70afd
james <imirzian@umich.edu>
2022-06-07 14:51:49 -0400

    revert headless for redapi

--- reporting/redapi.py
+++ reporting/redapi.py
@@ -72,7 +72,7 @@ class RedApi(object):
     def init_browser(self):
         download_path = os.path.join(os.getcwd(), 'tmp')
         co = wd.chrome.options.Options()
-        # co.headless = True
+        co.headless = True
         co.add_argument('--disable-features=VizDisplayCompositor')
         co.add_argument('--window-size=1920,1080')
         co.add_argument('--start-maximized')
