bd0c0e1d21e3b224271753f9c360858156ebf46a
jessicaleviton <levitonjn@gmail.com>
2023-03-02 18:50:18 -0500

    Build params and url based on fields argument. Parse datetime to string. Allow country filters.
    Co-authored-by: mrichardson <mlrichardson07@yahoo.com>

--- reporting/nzapi.py
+++ reporting/nzapi.py
@@ -9,15 +9,18 @@ import datetime as dt
 import reporting.utils as utl
 
 
-class NzAPI(object):
+class NzApi(object):
     config_path = utl.config_path
-    base_url = "https://api.newzoo.com/public/engagement/games/data"
+    base_url = "https://api.newzoo.com/public"
+    engagement_url = "/engagement"
+    games_url = '/games/data'
 
     def __init__(self):
         self.config = None
         self.config_file = None
         self.game_title = None
         self.api_key = None
+        self.country_filter = None
         self.config_list = None
         self.df = pd.DataFrame()
         self.r = None
@@ -41,6 +44,8 @@ class NzAPI(object):
             sys.exit(0)
         self.game_title = self.config['game_title']
         self.api_key = self.config['api_key']
+        if 'country_filter' in self.config:
+            self.country_filter = self.config['country_filter']
         self.config_list = [self.config, self.api_key, self.game_title]
 
     def check_config(self):
@@ -53,14 +58,10 @@ class NzAPI(object):
     def get_data(self, sd=None, ed=None, fields=None):
         sd, ed = self.get_data_default_check(sd, ed)
         header = self.create_header()
-        params = {
-            'start_date': sd,
-            'end_date': ed,
-            'games': [self.game_title]
-        }
-        self.r = self.make_request('get', self.base_url, params=params,
+        url, params = self.parse_fields(sd, ed, fields)
+        self.r = self.make_request('get', url, params=params,
                                    header=header)
-        self.df = pd.DataFrame(self.r)
+        self.df = pd.DataFrame(self.r.json())
         return self.df
 
     @staticmethod
@@ -70,6 +71,20 @@ class NzAPI(object):
         if ed is None:
             ed = dt.datetime.today()
         return sd, ed
+
+    def parse_fields(self, sd, ed, fields):
+        engage_url = self.engagement_url
+        category_url = self.games_url
+        params = {
+            'start_date': sd.strftime("%Y-%m-%d"),
+            'end_date': ed.strftime("%Y-%m-%d"),
+            'games': [self.game_title]
+        }
+        if self.country_filter:
+            params['geo_type'] = 'markets'
+            params['markets'] = self.country_filter.split(',')
+        url = self.base_url + engage_url + category_url
+        return url, params
 
     def make_request(self, method, url, params=None, body=None, header=None):
         try:
