5a901f265c9954cf663eff5043ee7792d47d3fd4
gdaudlin <gdaudlin@liquidarcade.com>
2023-10-31 14:29:03 -0400

    small edits

--- reporting/utils.py
+++ reporting/utils.py
@@ -66,7 +66,7 @@ def import_read_csv(filename, path=None, file_check=True, error_bad='error',
     else:
         read_func = pd.read_csv
         kwargs['encoding'] = 'utf-8'
-        kwargs['error_bad_lines'] = error_bad
+        kwargs['on_bad_lines'] = error_bad
     try:
         df = read_func(filename, **kwargs)
     except UnicodeDecodeError:
