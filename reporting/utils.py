a2b9077312f3e826cc89c7c9de9ebf1d28852c13
james <imirzian@umich.edu>
2023-11-09 16:50:47 -0500

    attempt catch month day switch in string_to_date

--- reporting/utils.py
+++ reporting/utils.py
@@ -152,7 +152,10 @@ def string_to_date(my_string):
         return dt.datetime.strptime(my_string, '%a %b %d %M:%S:%H %Y')
     elif (('-' in my_string) and (my_string[:2] == '20') and
           len(my_string) == 10):
-        return dt.datetime.strptime(my_string, '%Y-%m-%d')
+        try:
+            return dt.datetime.strptime(my_string, '%Y-%m-%d')
+        except ValueError:
+            return dt.datetime.strptime(my_string, '%Y-%d-%m')
     elif ((len(my_string) == 19) and (my_string[:2] == '20') and
           ('-' in my_string) and (':' in my_string)):
         return dt.datetime.strptime(my_string, '%Y-%m-%d %H:%M:%S')
