f7be539025ea300a0dfcabb585912399661a2d9a
james <imirzian@umich.edu>
2024-06-05 12:53:15 -0400

    fix api_sim_key vm reference

--- reporting/vendormatrix.py
+++ reporting/vendormatrix.py
@@ -65,7 +65,7 @@ class VendorMatrix(object):
             vmc.api_nz_key: [],
             vmc.api_ytd_key: [],
             vmc.api_wal_key: [],
-            vmc.api.sim_key: []
+            vmc.api_sim_key: []
         }
         self.ftp_sz_key = []
         self.db_dna_key = []
