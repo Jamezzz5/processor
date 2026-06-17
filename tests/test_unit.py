7c21720253c932ebaaec1cf1d772d40dd1fad716
chloehb <73759459+chloehb@users.noreply.github.com>
2026-06-17 12:23:59 -0500

    Remove file reliance for test_flat_fix (#393)
    * write base df to translation dict to prevent reliance on pre-existing file for test_flat_fix
    
    * switch hard coded strings to variables
    
    * add check for if df empty

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -857,6 +857,16 @@ class TestAnalyze:
         first_click_date = '2022-07-25'
         cfs = az.CheckFlatSpends(az.Analyze())
         translation = dct.DictTranslationConfig()
+        if translation.df.empty:
+            translation.df = pd.DataFrame({
+                dctc.DICT_COL_NAME: [],
+                dctc.DICT_COL_VALUE: [],
+                dctc.DICT_COL_NVALUE: [],
+                dctc.DICT_COL_FNC: [],
+                dctc.DICT_COL_SEL: [],
+                'index': []
+            })
+            translation.write(translation.df, dctc.filename_tran_config)
         df = pd.DataFrame({
             dctc.VEN: ['IMGN'],
             dctc.COU: ['US'],
